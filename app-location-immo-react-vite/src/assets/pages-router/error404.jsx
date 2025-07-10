e2e421e760f8c581249c1868bb16da4ec6d96d49
CynthiaCG-2202 <cynthiacrosagravez@gmail.com>
2025-07-10 16:56:06 +0200

    page 404 ajoutée avec le style

--- app-location-immo-react-vite/src/assets/pages-router/error404.jsx
+++ app-location-immo-react-vite/src/assets/pages-router/error404.jsx
@@ -1,9 +1,20 @@
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+
 function Error404() {
   return (
     <main className="error-page">
-      <h1>404</h1>
-      <p>Oups ! La page que vous demandez n'existe pas.</p>
+      <div className="container-accueil">
+        <Header />
+        </div>
+      <div className="main-container-404">
+        <div className="error-name">404</div>
+      <div className="subtitle-error">Oups ! La page que vous demandez n'existe pas.</div>
       <a href="/">Retourner sur la page d’accueil</a>
+      </div>
+
+      <Footer/>
     </main>
   );
 }
